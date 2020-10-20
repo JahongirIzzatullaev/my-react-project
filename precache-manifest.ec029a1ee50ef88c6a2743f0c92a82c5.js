ce396e0998603287ad65d48b976c820d547d2300
JahongirIzzatullaev@m <jahongirizzatullaev@mail.ru>
2020-10-21 01:13:21 +0500

    Updates

---
+++ precache-manifest.ec029a1ee50ef88c6a2743f0c92a82c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a2c50f1ba64895cd5178859c232e60",
+    "url": "/my-react-project/index.html"
+  },
+  {
+    "revision": "3b89b26cf957ba89a6a4",
+    "url": "/my-react-project/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "32fca20ca632184d224c",
+    "url": "/my-react-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b89b26cf957ba89a6a4",
+    "url": "/my-react-project/static/js/2.59107471.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-react-project/static/js/2.59107471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fca20ca632184d224c",
+    "url": "/my-react-project/static/js/main.0ebe65e8.chunk.js"
+  },
+  {
+    "revision": "9a7f94c20cc811d3add2",
+    "url": "/my-react-project/static/js/runtime-main.6547b7c2.js"
+  }
+]);
